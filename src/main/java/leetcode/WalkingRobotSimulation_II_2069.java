dc3efd9b86617a01d203de90ed3f41272a010526
Himanshu Karma <himanshukarma377@gmail.com>
2026-04-08 00:36:58 +0530

    feat: add Walking Robot Simulation II implementation with basic structure

---
+++ src/main/java/leetcode/WalkingRobotSimulation_II_2069.java
@@ -0,0 +1,30 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/walking-robot-simulation-ii/description/?envType=daily-question&envId=2026-04-07
+ */
+public class WalkingRobotSimulation_II_2069 {
+    int width;
+    int height;
+
+    public WalkingRobotSimulation_II_2069(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Walking Robot Simulation II");
+    }
+
+    public void step(int num) {
+
+    }
+
+    public int[] getPos() {
+        return null;
+    }
+
+    public String getDir() {
+        return null;
+    }
+}
