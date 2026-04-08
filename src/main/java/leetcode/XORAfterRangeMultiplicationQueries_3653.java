1f6c9b0178054c0e3e9d17949502aec562fe8594
Himanshu Karma <himanshukarma377@gmail.com>
2026-04-09 02:26:14 +0530

    feat: add initial implementation for XOR After Range Multiplication Queries

---
+++ src/main/java/leetcode/XORAfterRangeMultiplicationQueries_3653.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+/**
+ * LeetCode 3653. XOR After Range Multiplication Queries
+ * <p>
+ * https://leetcode.com/problems/xor-after-range-multiplication-queries-i/description/?envType=daily-question&envId=2026-04-08
+ */
+public class XORAfterRangeMultiplicationQueries_3653 {
+    public static void main(String[] args) {
+        int[] nums = {1, 1, 1};
+        int[][] queries = {{0, 2, 1, 4}};
+        System.out.println("Answer " + xorAfterQueries(nums, queries));
+    }
+
+    public static int xorAfterQueries(int[] nums, int[][] queries) {
+
+        return 0;
+    }
+}
