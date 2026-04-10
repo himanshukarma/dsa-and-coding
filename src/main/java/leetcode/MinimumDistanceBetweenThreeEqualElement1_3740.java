df84175d7788d5d33a9c3a1e1edae9d01c069790
Himanshu Karma <himanshukarma377@gmail.com>
2026-04-11 00:26:10 +0530

    feat: add MinimumDistanceBetweenThreeEqualElement1 implementation

---
+++ src/main/java/leetcode/MinimumDistanceBetweenThreeEqualElement1_3740.java
@@ -0,0 +1,12 @@
+package leetcode;
+
+public class MinimumDistanceBetweenThreeEqualElement1_3740 {
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4, 5};
+        System.out.println("Answer : " + minimumDistance(nums));
+    }
+
+    public static int minimumDistance(int[] nums) {
+        return 0;
+    }
+}
