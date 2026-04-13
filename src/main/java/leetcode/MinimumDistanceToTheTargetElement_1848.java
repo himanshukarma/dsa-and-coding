1734c74f64e3df1e0e1c634ffeeb4a0fcd46707b
Himanshu Karma <himanshukarma377@gmail.com>
2026-04-13 23:58:18 +0530

    feat: add MinimumDistanceToTheTargetElement implementation

---
+++ src/main/java/leetcode/MinimumDistanceToTheTargetElement_1848.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+public class MinimumDistanceToTheTargetElement_1848 {
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4, 5};
+        int target = 5;
+        int start = 3;
+
+        System.out.println(getMinDistance(nums, target, start)); // Output: 1
+    }
+
+    public static int getMinDistance(int[] nums, int target, int start) {
+
+        return 0;
+    }
+}
