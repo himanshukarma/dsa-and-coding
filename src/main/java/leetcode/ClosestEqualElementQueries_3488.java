ddf088f6dcc796b35d475ec3f2bd3b32f74253e0
Himanshu Karma <himanshukarma377@gmail.com>
2026-04-17 01:24:55 +0530

    feat: add ClosestEqualElementQueries implementation with initial structure

---
+++ src/main/java/leetcode/ClosestEqualElementQueries_3488.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+import java.util.List;
+
+/**
+ * https://leetcode.com/problems/closest-equal-element-queries/description/?envType=daily-question&envId=2026-04-16
+ */
+public class ClosestEqualElementQueries_3488 {
+    public static void main(String[] args) {
+        System.out.println("Result : ");
+    }
+
+    public static List<Integer> solveQueries(int[] nums, int[] queries) {
+        return null;
+    }
+}
