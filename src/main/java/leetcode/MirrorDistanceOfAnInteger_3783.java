eab38e5e31b36e208dcd403f12b05e8e21292e84
Himanshu Karma <himanshukarma377@gmail.com>
2026-04-19 02:54:20 +0530

    feat: implement mirrorDistance method and main method for testing

--- src/main/java/leetcode/MirrorDistanceOfAnInteger_3783.java
+++ src/main/java/leetcode/MirrorDistanceOfAnInteger_3783.java
@@ -1,4 +1,22 @@
 package leetcode;
 
 public class MirrorDistanceOfAnInteger_3783 {
+    public static void main(String[] args) {
+        System.out.println("Result : " + mirrorDistance(123));
+    }
+
+    public static int mirrorDistance(int n) {
+
+        return Math.abs(n - repeatNumber(n));
+    }
+
+    private static int repeatNumber(int n) {
+        int result = 0;
+        while (n > 0) {
+            int digit = n % 10;
+            result = result * 10 + digit;
+            n /= 10;
+        }
+        return result;
+    }
 }
