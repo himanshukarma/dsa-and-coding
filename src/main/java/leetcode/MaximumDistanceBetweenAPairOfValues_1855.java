4e2bc3a8c2327e6ef947fa0f51636d1599cb147d
Himanshu Karma <himanshukarma377@gmail.com>
2026-04-20 00:30:35 +0530

    feat: implement maxDistance method to calculate maximum distance between pairs

--- src/main/java/leetcode/MaximumDistanceBetweenAPairOfValues_1855.java
+++ src/main/java/leetcode/MaximumDistanceBetweenAPairOfValues_1855.java
@@ -2,14 +2,22 @@ package leetcode;
 
 public class MaximumDistanceBetweenAPairOfValues_1855 {
     public static void main(String[] args) {
-        int[] nums1 = {55,30,5,4,2};
-        int[] nums2 = {100,20,10,10,5};
+        int[] nums1 = {55, 30, 5, 4, 2};
+        int[] nums2 = {100, 20, 10, 10, 5};
 
         System.out.println("Result : " + maxDistance(nums1, nums2));
     }
 
     public static int maxDistance(int[] nums1, int[] nums2) {
+        int result = 0;
 
-        return 0;
+        for (int i = 0; i < nums1.length; i++) {
+            for (int j = 0; j < nums2.length; j++) {
+                if (nums1[i] <= nums2[j]) {
+                    result = Math.max(result, j - i);
+                }
+            }
+        }
+        return result;
     }
 }
