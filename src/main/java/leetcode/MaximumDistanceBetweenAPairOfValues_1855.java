648d7826302ddc8f299c249f8b2ee9dd32871cfa
Himanshu Karma <himanshukarma377@gmail.com>
2026-04-20 00:36:31 +0530

    feat: optimize maxDistance method for improved performance

--- src/main/java/leetcode/MaximumDistanceBetweenAPairOfValues_1855.java
+++ src/main/java/leetcode/MaximumDistanceBetweenAPairOfValues_1855.java
@@ -9,15 +9,12 @@ public class MaximumDistanceBetweenAPairOfValues_1855 {
     }
 
     public static int maxDistance(int[] nums1, int[] nums2) {
-        int result = 0;
+        int i, j = 1;
 
-        for (int i = 0; i < nums1.length; i++) {
-            for (int j = 0; j < nums2.length; j++) {
-                if (nums1[i] <= nums2[j]) {
-                    result = Math.max(result, j - i);
-                }
-            }
-        }
-        return result;
+        for (i = 0; i < nums1.length && j < nums2.length; j++)
+            if (nums1[i] > nums2[j])
+                i++;
+
+        return j - i - 1;
     }
 }
