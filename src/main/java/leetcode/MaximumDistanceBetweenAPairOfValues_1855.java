e1f2069894ba6c4d4aaacf66d268875f7c9cbe21
Himanshu Karma <himanshukarma377@gmail.com>
2026-04-20 00:24:10 +0530

    feat: add main method and initial maxDistance implementation in MaximumDistanceBetweenAPairOfValues class

--- src/main/java/leetcode/MaximumDistanceBetweenAPairOfValues_1855.java
+++ src/main/java/leetcode/MaximumDistanceBetweenAPairOfValues_1855.java
@@ -1,4 +1,15 @@
 package leetcode;
 
 public class MaximumDistanceBetweenAPairOfValues_1855 {
+    public static void main(String[] args) {
+        int[] nums1 = {55,30,5,4,2};
+        int[] nums2 = {100,20,10,10,5};
+
+        System.out.println("Result : " + maxDistance(nums1, nums2));
+    }
+
+    public static int maxDistance(int[] nums1, int[] nums2) {
+
+        return 0;
+    }
 }
