aee670c97734839652156b25e5de520307b5d178
Himanshu Karma <himanshukarma377@gmail.com>
2026-04-21 23:45:30 +0530

    feat: add MinimumHammingDistanceAfterSwapOperations class with main and initial method implementation

---
+++ src/main/java/leetcode/MinimumHammingDistanceAfterSwapOperations_1722.java
@@ -0,0 +1,14 @@
+package leetcode;
+
+public class MinimumHammingDistanceAfterSwapOperations_1722 {
+    public static void main(String[] args) {
+        int[] source = {1,2,3,4}, target = {2,1,4,5};
+        int[][] allowedSwaps = {{0,1},{2,3}};
+        System.out.println("Result : " + minimumHammingDistance(source, target, allowedSwaps));
+    }
+
+    public static int minimumHammingDistance(int[] source, int[] target, int[][] allowedSwaps) {
+
+        return 0;
+    }
+}
