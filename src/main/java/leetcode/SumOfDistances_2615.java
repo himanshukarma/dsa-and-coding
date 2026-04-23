fd5ca1682d401ba34d825499a492271e57b91c66
Himanshu Karma <himanshukarma377@gmail.com>
2026-04-24 00:14:14 +0530

    feat: add SumOfDistances_2615 class with distance method implementation

---
+++ src/main/java/leetcode/SumOfDistances_2615.java
@@ -0,0 +1,43 @@
+package leetcode;
+
+import java.util.*;
+
+public class SumOfDistances_2615 {
+    public static void main(String[] args) {
+        int[] nums = {1, 3, 1, 1, 2};
+        System.out.println("Result : " + Arrays.toString(distance(nums)));
+    }
+
+    public static long[] distance(int[] nums) {
+        int n = nums.length;
+        long[] ans = new long[n];
+
+        Map<Integer, List<Integer>> mp = new HashMap<>();
+
+        for (int i = 0; i < n; i++) {
+            mp.computeIfAbsent(nums[i], k -> new ArrayList<>()).add(i);
+        }
+
+        for (List<Integer> pos : mp.values()) {
+
+            long sum = 0;
+            for (int x : pos) sum += x;
+
+            long leftSum = 0;
+            int m = pos.size();
+
+            for (int i = 0; i < m; i++) {
+                long rightSum = sum - leftSum - pos.get(i);
+
+                long left = (long) pos.get(i) * i - leftSum;
+                long right = rightSum - (long) pos.get(i) * (m - i - 1);
+
+                ans[pos.get(i)] = left + right;
+
+                leftSum += pos.get(i);
+            }
+        }
+
+        return ans;
+    }
+}
