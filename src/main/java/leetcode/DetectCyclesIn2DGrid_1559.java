f7762af1af35209363a07ec73a95683b8a80a94d
Himanshu Karma <himanshukarma377@gmail.com>
2026-04-26 16:26:18 +0530

    feat: add DetectCyclesIn2DGrid_1559 class with containsCycle method skeleton

---
+++ src/main/java/leetcode/DetectCyclesIn2DGrid_1559.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+public class DetectCyclesIn2DGrid_1559 {
+
+    public static void main(String[] args) {
+        char[][] grid = {
+                {'a', 'a', 'a', 'a'},
+                {'a', 'b', 'b', 'a'},
+                {'a', 'b', 'b', 'a'},
+                {'a', 'a', 'a', 'a'}
+        };
+        System.out.println("Result : " + containsCycle(grid));
+    }
+
+    public static boolean containsCycle(char[][] grid) {
+
+        return false;
+    }
+}
