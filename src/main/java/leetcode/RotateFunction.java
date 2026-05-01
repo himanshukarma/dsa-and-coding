b75a75d82c1856d7247ba110f4bafb9aa1db345d
Himanshu Karma <himanshukarma377@gmail.com>
2026-05-02 02:23:02 +0530

    feat: added RotateFunction with method implementation

---
+++ src/main/java/leetcode/RotateFunction.java
@@ -0,0 +1,30 @@
+package leetcode;
+
+public class RotateFunction {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] nums = { 4, 3, 2, 6 };
+		System.out.println("Result : " + maxRotateFunction(nums));
+	}
+
+	public static int maxRotateFunction(int[] nums) {
+
+		int sum = 0, F = 0;
+		int n = nums.length;
+
+		for (int i = 0; i < n; i++) {
+			sum += nums[i];
+			F += i * nums[i];
+		}
+
+		int max = F;
+
+		for (int i = 1; i < n; i++) {
+			F += sum - n * nums[n - i];
+			max = Math.max(max, F);
+		}
+
+		return max;
+	}
+}
