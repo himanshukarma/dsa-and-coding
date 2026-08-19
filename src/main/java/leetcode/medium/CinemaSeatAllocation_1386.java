f46c65b7a78e5a5bb3197e4298cf434fe46d0641
Himanshu Karma <himanshukarma377@gmail.com>
2026-08-20 00:45:14 +0530

    feat: add CinemaSeatAllocation_1386 with initial implementation

---
+++ src/main/java/leetcode/medium/CinemaSeatAllocation_1386.java
@@ -0,0 +1,17 @@
+package leetcode.medium;
+
+/**
+ * CinemaSeatAllocation_1386
+ * 
+ * Topics Covered - 
+ * Senior
+ * Array
+ * HashTable
+ * Greedy
+ * Bit Manipulation
+ */
+public class CinemaSeatAllocation_1386 {
+    public static void main(String[] args) {
+        System.out.println("Worked");
+    }
+}
