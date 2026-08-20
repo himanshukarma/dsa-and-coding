472669d4d5650ef9734dbbd178dce0c69e16d873
Himanshu Karma <himanshukarma377@gmail.com>
2026-08-21 00:31:46 +0530

    feat: add DistributeElementsIntoTwoArraysI_3069 class with main method

---
+++ src/main/java/leetcode/easy/DistributeElementsIntoTwoArraysI_3069.java
@@ -0,0 +1,14 @@
+package leetcode.easy;
+
+/**
+ * https://leetcode.com/problems/distribute-elements-into-two-arrays-i/?envType=daily-question&envId=2026-08-20
+ * 
+ * Topics -
+ *  Array
+ *  Simulation
+ */
+public class DistributeElementsIntoTwoArraysI_3069 {
+    public static void main(String[] args) {
+        System.out.println("Distribute Elements Into Two Arrays I");
+    }
+}
