cace8193d7167f68605f22367d4a75684c5518f1
Himanshu Karma <himanshukarma377@gmail.com>
2026-08-21 23:54:25 +0530

    feat: add KthSmallestAmountWithSingleDenominationCombination_3116 class with main method

---
+++ src/main/java/leetcode/hard/KthSmallestAmountWithSingleDenominationCombination_3116.java
@@ -0,0 +1,7 @@
+package leetcode.hard;
+
+public class KthSmallestAmountWithSingleDenominationCombination_3116 {
+    public static void main(String[] args) {
+        System.out.println("Worked");
+    }
+}
