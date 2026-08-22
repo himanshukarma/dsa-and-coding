b51cb19e5c7a29e0cde40de02b70de9fb573d335
Himanshu Karma <himanshukarma377@gmail.com>
2026-08-22 13:26:18 +0530

    feat: add CheckDivisibilityByDigitSumAndProduct class with main method

---
+++ src/main/java/leetcode/easy/CheckDivisibilityByDigitSumAndProduct.java
@@ -0,0 +1,11 @@
+package leetcode.easy;
+
+public class CheckDivisibilityByDigitSumAndProduct {
+    public static void main(String[] args) {
+        System.out.println("Worked");
+    }
+
+    public boolean checkDivisibility(int n) {
+        return false;
+    }
+}
