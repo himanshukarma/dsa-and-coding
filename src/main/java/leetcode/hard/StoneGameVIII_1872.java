954d8178fb471800f7ac2884c05c0f35a6a32344
Himanshu Karma <himanshukarma377@gmail.com>
2026-08-24 23:48:54 +0530

    feat: implement stoneGameVIII method and update main for testing

--- src/main/java/leetcode/hard/StoneGameVIII_1872.java
+++ src/main/java/leetcode/hard/StoneGameVIII_1872.java
@@ -1,7 +1,28 @@
 package leetcode.hard;
 
+/**
+ * StoneGameVIII_1872
+ * 
+ * https://leetcode.com/problems/stone-game-viii/description/
+ * 
+ * Runtime - 3ms
+ * Memory - 83.3MB
+ */
 public class StoneGameVIII_1872 {
     public static void main(String[] args) {
-        System.out.println("Worked!");
+        int[] stones = {7, -6, 9, -3, -2};
+        System.out.println(stoneGameVIII(stones));
+    }
+
+    public static int stoneGameVIII(int[] stones) {
+        int n = stones.length;
+        for (int i = 1; i < n; i++)
+            stones[i] += stones[i - 1];
+
+        int ans = stones[n - 1];
+        for (int i = n - 2; i > 0; i--)
+            ans = Math.max(ans, stones[i] - ans);
+
+        return ans;
     }
 }
