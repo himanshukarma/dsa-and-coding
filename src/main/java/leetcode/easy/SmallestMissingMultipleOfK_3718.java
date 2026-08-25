64b5c0fe6b23a1a3f46b24a419dca6c5086dcc20
Himanshu Karma <himanshukarma377@gmail.com>
2026-08-25 23:51:40 +0530

    feat: implement missingMultiple method in SmallestMissingMultipleOfK_3718 class

--- src/main/java/leetcode/easy/SmallestMissingMultipleOfK_3718.java
+++ src/main/java/leetcode/easy/SmallestMissingMultipleOfK_3718.java
@@ -1,12 +1,32 @@
 package leetcode.easy;
 
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * SmallestMissingMultipleOfK_3718
+ * 
+ * https://leetcode.com/problems/smallest-missing-multiple-of-k
+ * 
+ * Runtime - 2ms
+ * Memory - 45.34 MB
+ */
 public class SmallestMissingMultipleOfK_3718 {
     public static void main(String[] args) {
-        System.out.println("Worked");
+        System.out.println(missingMultiple(new int[] { 8, 2, 3, 4, 6 }, 2)); // Output: 6
     }
 
     public static int missingMultiple(int[] nums, int k) {
+        Set<Integer> seen = new HashSet<>();
+        for (int num : nums) {
+            seen.add(num);
+        }
 
-        return 0;
+        int cur = k;
+        while (seen.contains(cur)) {
+            cur += k;
+        }
+
+        return cur;
     }
 }
