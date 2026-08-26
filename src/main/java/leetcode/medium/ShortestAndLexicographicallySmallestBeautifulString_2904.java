4edde4ff621304d2677ccd0cf2e4b93fdfcea1ca
Himanshu Karma <himanshukarma377@gmail.com>
2026-08-26 22:16:52 +0530

    feat: implement shortestBeautifulSubstring method to find the shortest beautiful substring

--- src/main/java/leetcode/medium/ShortestAndLexicographicallySmallestBeautifulString_2904.java
+++ src/main/java/leetcode/medium/ShortestAndLexicographicallySmallestBeautifulString_2904.java
@@ -2,6 +2,42 @@ package leetcode.medium;
 
 public class ShortestAndLexicographicallySmallestBeautifulString_2904 {
     public static void main(String[] args) {
-        System.out.println("Worked");
+        System.out.println(shortestBeautifulSubstring("001101", 2)); // Output: "110"
+    }
+
+    public static String shortestBeautifulSubstring(String s, int k) {
+        String answer = "";
+        int left = 0;
+        int ones = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+            if (s.charAt(right) == '1') {
+                ones++;
+            }
+
+            while (ones > k) {
+                if (s.charAt(left) == '1') {
+                    ones--;
+                }
+                left++;
+            }
+
+            while (ones == k && s.charAt(left) == '0') {
+                left++;
+            }
+
+            if (ones == k) {
+                String candidate = s.substring(left, right + 1);
+
+                if (answer.isEmpty() ||
+                    candidate.length() < answer.length() ||
+                    (candidate.length() == answer.length() &&
+                     candidate.compareTo(answer) < 0)) {
+                    answer = candidate;
+                }
+            }
+        }
+
+        return answer;
     }
 }
