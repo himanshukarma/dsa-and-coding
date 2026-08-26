deb9e1057cb2266ec6d5784c3fe5e9e608d641e7
Himanshu Karma <himanshukarma377@gmail.com>
2026-08-26 22:08:53 +0530

    feat: add ShortestAndLexicographicallySmallestBeautifulString_2904 class with main method for initial setup

---
+++ src/main/java/leetcode/medium/ShortestAndLexicographicallySmallestBeautifulString_2904.java
@@ -0,0 +1,7 @@
+package leetcode.medium;
+
+public class ShortestAndLexicographicallySmallestBeautifulString_2904 {
+    public static void main(String[] args) {
+        System.out.println("Worked");
+    }
+}
