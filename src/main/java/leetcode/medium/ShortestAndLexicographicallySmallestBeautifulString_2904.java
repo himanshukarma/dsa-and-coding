ff6c2325203749dde0fec2e3792be29c645d115a
Himanshu Karma <himanshukarma377@gmail.com>
2026-08-26 22:18:42 +0530

    style: update class documentation for ShortestAndLexicographicallySmallestBeautifulString_2904

--- src/main/java/leetcode/medium/ShortestAndLexicographicallySmallestBeautifulString_2904.java
+++ src/main/java/leetcode/medium/ShortestAndLexicographicallySmallestBeautifulString_2904.java
@@ -1,5 +1,13 @@
 package leetcode.medium;
 
+/**
+ * ShortestAndLexicographicallySmallestBeautifulString_2904
+ * 
+ * https://leetcode.com/problems/shortest-and-lexicographically-smallest-beautiful-string
+ * 
+ * Runtime - 1ms
+ * Memory - 44.06 MB
+ */
 public class ShortestAndLexicographicallySmallestBeautifulString_2904 {
     public static void main(String[] args) {
         System.out.println(shortestBeautifulSubstring("001101", 2)); // Output: "110"
