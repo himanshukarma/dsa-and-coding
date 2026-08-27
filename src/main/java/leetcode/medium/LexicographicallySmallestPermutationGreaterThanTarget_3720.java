7c7694b5b52ab8aae2195f1b5ed7687abe610a0d
Himanshu Karma <himanshukarma377@gmail.com>
2026-08-28 00:02:53 +0530

    feat: add LexicographicallySmallestPermutationGreaterThanTarget_3720 class with main method for initial setup

---
+++ src/main/java/leetcode/medium/LexicographicallySmallestPermutationGreaterThanTarget_3720.java
@@ -0,0 +1,7 @@
+package leetcode.medium;
+
+public class LexicographicallySmallestPermutationGreaterThanTarget_3720 {
+    public static void main(String[] args) {
+        System.out.println("Worked!");
+    }
+}
