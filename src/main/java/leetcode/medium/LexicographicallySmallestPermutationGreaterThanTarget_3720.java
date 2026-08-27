cc76877ec3853dda7409cfad8f39d2674240e676
Himanshu Karma <himanshukarma377@gmail.com>
2026-08-28 00:08:19 +0530

    feat: implement lexGreaterPermutation method to find the lexicographically smallest permutation greater than target

--- src/main/java/leetcode/medium/LexicographicallySmallestPermutationGreaterThanTarget_3720.java
+++ src/main/java/leetcode/medium/LexicographicallySmallestPermutationGreaterThanTarget_3720.java
@@ -2,6 +2,60 @@ package leetcode.medium;
 
 public class LexicographicallySmallestPermutationGreaterThanTarget_3720 {
     public static void main(String[] args) {
-        System.out.println("Worked!");
+        String s = "abc";
+        String target = "bba";
+        System.out.println(lexGreaterPermutation(s, target));
+    }
+
+    public static String lexGreaterPermutation(String s, String target) {
+        int[] cnt = new int[26];
+
+        for (char ch : s.toCharArray()) {
+            cnt[ch - 'a']++;
+        }
+
+        for (char ch : target.toCharArray()) {
+            cnt[ch - 'a']--;
+        }
+
+        for (int i = target.length() - 1; i >= 0; i--) {
+            int cur = target.charAt(i) - 'a';
+            cnt[cur]++;
+
+            boolean ok = true;
+            for (int x : cnt) {
+                if (x < 0) {
+                    ok = false;
+                    break;
+                }
+            }
+
+            if (!ok) continue;
+
+            int next = -1;
+            for (int c = cur + 1; c < 26; c++) {
+                if (cnt[c] > 0) {
+                    next = c;
+                    break;
+                }
+            }
+
+            if (next == -1) continue;
+
+            cnt[next]--;
+
+            StringBuilder ans = new StringBuilder(target.substring(0, i));
+            ans.append((char) ('a' + next));
+
+            for (int c = 0; c < 26; c++) {
+                while (cnt[c]-- > 0) {
+                    ans.append((char) ('a' + c));
+                }
+            }
+
+            return ans.toString();
+        }
+
+        return "";
     }
 }
