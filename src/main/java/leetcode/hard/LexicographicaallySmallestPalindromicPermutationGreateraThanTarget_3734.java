59da77e922a4ad448230673b7739f5efb1ce6613
Himanshu Karma <himanshukarma377@gmail.com>
2026-08-28 21:12:08 +0530

    feat: add LexicographicallySmallestPalindromicPermutationGreaterThanTarget_3734 class with main method for initial setup

---
+++ src/main/java/leetcode/hard/LexicographicaallySmallestPalindromicPermutationGreateraThanTarget_3734.java
@@ -0,0 +1,7 @@
+package leetcode.hard;
+
+public class LexicographicaallySmallestPalindromicPermutationGreateraThanTarget_3734 {
+    public static void main(String[] args) {
+        System.out.println("Worked!");
+    }
+}
