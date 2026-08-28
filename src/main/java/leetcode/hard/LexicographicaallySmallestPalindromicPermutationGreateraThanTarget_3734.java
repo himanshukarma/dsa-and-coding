37753bbe214aee1e2077f1125d4a725c3df3fffc
Himanshu Karma <himanshukarma377@gmail.com>
2026-08-28 21:12:44 +0530

    feat: update main method to demonstrate lexPalindromicPermutation functionality

--- src/main/java/leetcode/hard/LexicographicaallySmallestPalindromicPermutationGreateraThanTarget_3734.java
+++ src/main/java/leetcode/hard/LexicographicaallySmallestPalindromicPermutationGreateraThanTarget_3734.java
@@ -2,6 +2,10 @@ package leetcode.hard;
 
 public class LexicographicaallySmallestPalindromicPermutationGreateraThanTarget_3734 {
     public static void main(String[] args) {
-        System.out.println("Worked!");
+        System.out.println(lexPalindromicPermutation("aabb", "abba")); // Output: "baab"
+    }
+
+    public static String lexPalindromicPermutation(String s, String target) {
+        return "";
     }
 }
