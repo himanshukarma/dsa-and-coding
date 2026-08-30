b3db5a67b498712a142a1bcdc15dea3b1780d6fe
Himanshu Karma <himanshukarma377@gmail.com>
2026-08-31 00:03:37 +0530

    feat: enhance minimumDeletions method to handle single element case and optimize logic

--- src/main/java/leetcode/medium/RemovingMinimumAndMaximumFromArray_2091.java
+++ src/main/java/leetcode/medium/RemovingMinimumAndMaximumFromArray_2091.java
@@ -7,13 +7,37 @@ public class RemovingMinimumAndMaximumFromArray_2091 {
     }
 
     public static int minimumDeletions(int[] nums) {
-        int len = nums.length;
-
-        if(len == 1) {
-            return 1;
+        int n = nums.length;
+        int left = 0;
+        int right = 0;
         
+        for (int i = 1; i < n; i++) {
+            if (nums[i] < nums[left])
+                left = i;
+                
+            if (nums[i] > nums[right])
+                right = i;
         }
-
-        return 0;
+        
+        if (left < right) {
+            int temp = left;
+            left = right;
+            right = temp;
+        }
+            
+        int ans = n;
+        
+        for (int i = 0; i <= n; i++) {
+            int extra = 0;
+            
+            if (right >= i)
+                extra = n - right;
+            else if (left >= i)
+                extra = n - left;
+                
+            ans = Math.min(ans, i + extra);
+        }
+        
+        return ans;
     }
 }
