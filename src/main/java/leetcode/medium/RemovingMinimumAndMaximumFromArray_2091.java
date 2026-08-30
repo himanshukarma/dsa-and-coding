1777c04b2f02e37901e41e66d2a5e6980b20df8d
Himanshu Karma <himanshukarma377@gmail.com>
2026-08-30 23:59:15 +0530

    feat: implement minimumDeletions method and update main for testing

--- src/main/java/leetcode/medium/RemovingMinimumAndMaximumFromArray_2091.java
+++ src/main/java/leetcode/medium/RemovingMinimumAndMaximumFromArray_2091.java
@@ -2,6 +2,18 @@ package leetcode.medium;
 
 public class RemovingMinimumAndMaximumFromArray_2091 {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        int[] nums = {2, 10, 7, 5, 4, 1, 8, 6};
+        System.out.println(minimumDeletions(nums));
+    }
+
+    public static int minimumDeletions(int[] nums) {
+        int len = nums.length;
+
+        if(len == 1) {
+            return 1;
+        
+        }
+
+        return 0;
     }
 }
