d1f80b054d479f641473ab1004584b167b14a02f
Himanshu Karma <himanshukarma377@gmail.com>
2026-08-30 23:24:33 +0530

    feat: add RemovingMinimumAndMaximumFromArray_2091 class with main method for initial setup

---
+++ src/main/java/leetcode/medium/RemovingMinimumAndMaximumFromArray_2091.java
@@ -0,0 +1,7 @@
+package leetcode.medium;
+
+public class RemovingMinimumAndMaximumFromArray_2091 {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
