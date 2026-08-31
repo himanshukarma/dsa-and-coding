34e7ee7c23771dd626b843383216f8ad817ef62f
Himanshu Karma <himanshukarma377@gmail.com>
2026-08-31 23:43:09 +0530

    feat: add FindTheMinimumAndMaximumNumberOfNodesBetweenCriticalPoints_2058 class with main method for initial setup

---
+++ src/main/java/leetcode/medium/FindTheMinimumAndMaximumNumberOfNodesBetweenCriticalPoints_2058.java
@@ -0,0 +1,7 @@
+package leetcode.medium;
+
+public class FindTheMinimumAndMaximumNumberOfNodesBetweenCriticalPoints_2058 {
+    public static void main(String[] args) {
+        System.out.println("Worked");
+    }
+}
