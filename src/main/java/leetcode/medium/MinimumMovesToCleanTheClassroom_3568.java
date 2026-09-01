ffd026a136b69d79b792d5466205af3d37896524
Himanshu Karma <himanshukarma377@gmail.com>
2026-09-01 23:04:26 +0530

    feat: update main method to call minMoves with sample input

--- src/main/java/leetcode/medium/MinimumMovesToCleanTheClassroom_3568.java
+++ src/main/java/leetcode/medium/MinimumMovesToCleanTheClassroom_3568.java
@@ -2,6 +2,10 @@ package leetcode.medium;
 
 public class MinimumMovesToCleanTheClassroom_3568 {
     public static void main(String[] args) {
-        System.out.println("Worked");
+        System.out.println(minMoves(new String[] { "S.", "XL" }, 2));
+    }
+
+    public static int minMoves(String[] classroom, int energy) {
+        return 0;
     }
 }
