5a0f6ca4995f413b14f4cac49b7db79ee9c5c6bd
Himanshu Karma <himanshukarma377@gmail.com>
2026-09-02 23:58:43 +0530

    feat: update main method to call uniformArray with sample input

--- src/main/java/leetcode/easy/ConstructUniformParityArrayI_3875.java
+++ src/main/java/leetcode/easy/ConstructUniformParityArrayI_3875.java
@@ -2,6 +2,11 @@ package leetcode.easy;
 
 public class ConstructUniformParityArrayI_3875 {
     public static void main(String[] args) {
-        System.out.println("Worked!");
+        int nums[] = { 2, 4 };
+        System.out.println(uniformArray(nums));
+    }
+
+    public static boolean uniformArray(int[] nums1) {
+        return true;
     }
 }
