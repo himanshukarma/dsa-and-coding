b13094acf98f3ccf90742d47951da66493da373b
Himanshu Karma <himanshukarma377@gmail.com>
2026-09-03 00:18:15 +0530

    feat: implement gcdOfStrings method to calculate GCD of two strings

--- src/main/java/leetcode75/GreatestCommonDivisorOfString_1071.java
+++ src/main/java/leetcode75/GreatestCommonDivisorOfString_1071.java
@@ -2,6 +2,31 @@ package leetcode75;
 
 public class GreatestCommonDivisorOfString_1071 {
     public static void main(String[] args) {
-        System.out.println("Worked!");
+        String str1 = "ABCABC", str2 = "ABC";
+        System.out.println(gcdOfStrings(str1, str2));
+    }
+
+    public static String gcdOfStrings(String str1, String str2) {
+        // Step 1: Check if a common pattern exists
+        if (!(str1 + str2).equals(str2 + str1)) {
+            return "";
+        }
+
+        // Step 2: Find GCD of the lengths
+        int length = gcd(str1.length(), str2.length());
+
+        // Step 3: Return prefix of that length
+        return str1.substring(0, length);
+    }
+
+    private static int gcd(int a, int b) {
+
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+
+        return a;
     }
 }
